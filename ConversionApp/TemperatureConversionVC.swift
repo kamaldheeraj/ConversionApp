f84437156286a494f16cb95e935e7251bd9598dd
Pravin Kandala <z1761257@CSCImini13.local>
2015-10-07 17:44:55 -0500

    Temperature conversion.swift file added !

--- ConversionApp/TemperatureConversionVC.swift
+++ ConversionApp/TemperatureConversionVC.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class TemperatureConversionVC: UIViewController {
+    var values = [Int]()
+    
+    var part = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,63 @@ class TemperatureConversionVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//code goes here
+    
+    @IBOutlet weak var segmentedControl2: UISegmentedControl!
+    @IBOutlet weak var pickerTemperature: UIPickerView!
+    @IBOutlet weak var outLabel: UILabel!
+    
+    
+    @IBAction func segmentedControlChanged2(sender: AnyObject) {
+        
+        if sender.selectedSegmentIndex == 0{
+            
+            values = [1, 2, 3]
+            
+            
 
+            part = 1
+            
+            //F to C code goes here
+        }
+        else
+        {
+            
+            values = [4,5,6]
+            
+            part = 2
+            //C to F code goes here
+            
+        }
+        
+    }
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerTemperature(_pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return values.count
+    }
+    
+    func pickerTemperature(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return values[row].description
+        
+    }
+    func pickerTemperature(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        var newTemperature = 0.0
+        
+            var temperature = values[row]
+        
+        
+        newTemperature = Double(temperature)
+        
+        outLabel.text = newTemperature.description
+
+        
+    }
+    
+    
 }
 
